f7be1fc634020bc1a4e13d62cf583da662e64a18
xEm1rald <rostikroi2009@gmail.com>
2026-03-16 09:06:44 +0200

    fix init

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-import asyncio
-import csv
-import threading
-
 from bot import bot
 
 
-
-async def main():
-    # #hsn = "P250 | X-Ray (Factory New)"
-    # hsn = "Sticker | Fluxo (Holo) | Austin 2025"
-    #
-    # #await parse_price_history(hsn)
-    # await parse_item_orders(hsn)
-    ...
-
-
 if __name__ == '__main__':
-    threading.Thread(target=bot.run).run()
-    asyncio.run(main())
+    bot.run()
